25f6399236bc90cc1a7ee98d887b47139abbbd84
zogreptile <zogreptile@gmail.com>
2018-08-07 00:19:46 +1000

    Exclude logic from bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-import { version } from '../../package.json';
+import gendiff from '..';
 
-const program = require('commander');
-
-program
-  .version(version)
-  .arguments('<firstConfig> <secondConfig>')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format')
-  .parse(process.argv);
+gendiff();
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { version } from '../package.json';
+
+const program = require('commander');
+
+const core = () => program
+  .version(version)
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
+
+export default core;
