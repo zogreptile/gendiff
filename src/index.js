076ba5b00da4a903a7d9abe5f3337637ede9a6c7
zogreptile <zogreptile@gmail.com>
2018-08-07 23:28:00 +1000

    Added YAML parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
 import { has } from 'lodash';
 
+const parsers = {
+  '.json': JSON.parse,
+  '.yaml': yaml.safeLoad,
+  '.yml': yaml.safeLoad,
+};
+
+const parseFile = (file) => {
+  const extension = path.extname(file);
+  const parser = parsers[extension];
+  return parser(fs.readFileSync(file, 'utf-8'));
+};
+
 const compare = (file1, file2) => {
-  const f1 = JSON.parse(fs.readFileSync(file1, 'utf-8'));
-  const f2 = JSON.parse(fs.readFileSync(file2, 'utf-8'));
+  const f1 = parseFile(file1);
+  const f2 = parseFile(file2);
 
   const deletedItems = Object.keys(f1).reduce((acc, key) =>
     (!has(f2, key) ? [...acc, `  - ${key}: ${f1[key]}`] : [...acc]), []);
