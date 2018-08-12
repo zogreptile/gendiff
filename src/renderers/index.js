6bebc87a2fffb7324b53f8a7cea85eff7cd2d6b0
zogreptile <zogreptile@gmail.com>
2018-08-12 20:18:35 +1000

    Added json renderer

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,9 +1,11 @@
 import prettyRenderer from './pretty-renderer';
 import plainRenderer from './plain-renderer';
+import jsonRenderer from './json-renderer';
 
 const renderFormats = {
   pretty: prettyRenderer,
   plain: plainRenderer,
+  json: jsonRenderer,
 };
 
 export default (ast, format) =>
